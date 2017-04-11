223219bfa256d989fad422981d74c31ac8f562be
rlukin <lknrmn@gmail.com>
2017-04-11 17:17:21 +0300

    add multiple checks

--- email_validator.sh
+++ email_validator.sh
@@ -5,21 +5,27 @@
 # #######################
 
 # Description
-# ---------------------------------------------------
+# ------------------------------------------------------
 # This script validate input string(s) for email address
-# ---------------------------------------------------
+# ------------------------------------------------------
 
 # Usefull links
 # -----------------------------------
 # https://tools.ietf.org/html/rfc2822
 # http://emailregex.com/
 # https://habrahabr.ru/post/224623/
+# http://rumkin.com/software/email/rules.php
 # -----------------------------------
+
+# Author
+# -----------
+# Roman Lukin
+# -----------
 
 
 # Print usage
 usage() {
- echo -n "  Usage:
+  echo -n "  Usage:
  $0 [OPTION] [STRINGS]
 
  Description:
@@ -35,32 +41,55 @@ usage() {
 }
 
 
-#regexp='(?:[a-z0-9!#$%&'
-#regexp+="'"
-#regexp+='*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'
-#regexp+="'"
-#regexp+='*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
-
 regexp='\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b'
 
+failed(){
+	echo -e "\t\e[31;1mFail\e[0m"
+}
+
+passed(){
+	echo -e "\t\e[32;1mOK\e[0m"
+}
 validate() {
-if [[ $1 =~ $regexp ]]; then
-  echo -e "\e[32;1m$1\e[0m seems valid"
-else
-  echo -e "\e[31;1m$1\e[0m not valid"
-fi
+  echo -e "Validating \e[32;1m$1\e[0m:\n"
+  #check domain existence (RFC2822 section 2.2)
+  echo -n "Domain check"
+  if [[ $1 =~ ^.+@[^@]+$ ]]; then
+	passed
+  else 
+	failed
+  fi
+  #check correctness for quotes (RFC 2822 section 3.4.1, 4.4)
+  #check periods (RFC 2822 section 3.4.1)
+  #check allowed symbols (RFC 2822 section 3.4.1)
+  #brackets for domain (RFC 2822 section 3.4.1)
+  #domain correctness (RFC 1035 2.3.4)
+  #check allowed length (RFC 1035 section 2.3.4, RFC 2821 section 4.5.3.1)
+  #check hyphens (RFC 1035, section 2.3.4)
+  #check for a and mx records (RFC 2821, section 3.6)
+  #
+
+  if [[ $1 =~ $regexp ]]; then
+	echo -e "\e[32;1m$1\e[0m seems valid"
+  else
+	echo -e "\e[31;1m$1\e[0m not valid"
+  fi
+  echo "--------------------------------------------------"
 }
 
 # Read the options
-if [[ $# -eq 0 ]]; then
-  usage
-elif [[ $1 =~ -h|--help ]]; then
-  usage >&2
-else
-  while (( "$#" )); do
-  validate "$1"
-  shift
-  done
-fi
+main(){
+  if [[ $# -eq 0 ]]; then
+	usage
+  elif [[ $1 =~ -h|--help ]]; then
+	usage >&2
+  else
+	while (( "$#" )); do
+	validate "$1"
+	shift
+	done
+  fi
+}
 
+main $@
 exit 0
